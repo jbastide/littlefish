e0fdd5722ae0b8af656176c7010e34a6149d3a18
Jesse Bastide <jbastide001@yahoo.com>
2015-02-07 15:42:17 +0100

    updated the script to output the device name

--- diskperf.rb
+++ diskperf.rb
@@ -1,7 +1,10 @@
 #
 # Connect directly to an ESX host and get performance manager metrics related to disk performance
+# TODO: Add json output to feed this data to graphite.
+# 
+# References: (Get the 5.5 API docs if you want to look for changes. But these counters are a start.
+# https://www.vmware.com/support/developer/vc-sdk/visdk400pubs/ReferenceGuide/disk_counters.html
 #
-
 
 require 'trollop'
 require 'rbvmomi'
@@ -35,12 +38,13 @@ password = opts[:password]
 #
 
 perfProperties = [{:name => "deviceLatency", :id => nil, :values => []}, 
-  {:name => "deviceReadLatency", :id => nil, :values => []},
-  {:name => "deviceWriteLatency", :id => nil, :values => []}, 
-  {:name => "kernelReadLatency", :id => nil, :values => []},
-  {:name => "kernelWriteLatency", :id => nil, :values => []}, 
-  {:name => "kernelLatency", :id => nil, :values => []}, 
-  {:name => "totalLatency", :id => nil, :values => []}, 
+  {:name => "deviceReadLatency", :units => 'milliseconds', :id => nil, :values => []},
+  {:name => "deviceWriteLatency", :units => 'milliseconds', :id => nil, :values => []}, 
+  {:name => "kernelReadLatency", :units => 'milliseconds', :id => nil, :values => []},
+  {:name => "kernelWriteLatency", :units => 'milliseconds', :id => nil, :values => []}, 
+  {:name => "kernelLatency", :units => 'milliseconds', :id => nil, :values => []}, 
+  {:name => "totalLatency", :units => 'milliseconds', :id => nil, :values => []}, 
+  {:name => "commands", :units => 'SCSI commands/second', :id => nil, :values => []}, 
 ] 
   
 
@@ -113,7 +117,7 @@ host = vim.serviceInstance.find_datacenter.hostFolder.children.first.host[0]
 #
 
 perfProperties.each { |entry|
-  metrics = [] # {:timeStamp => int, :units => str, :metric => int}
+  metrics = [] # {:timeStamp => int, :units => str, :metric => int, :device => str}
   metricId = entry[:id]
 
   #
@@ -128,8 +132,20 @@ perfProperties.each { |entry|
     metricId: [{counterId: metricId, instance: '*'}],
   )
 
-  # This is an array, but it only has a single (large) item.
+  # This is an array, but it only has a single (large) item of return data.
   result = pm.QueryPerf({querySpec: [pqs]})
+
+
+  #result.each {|r| r.value.each {|s| puts "DEBUG #{s._get_property(:id)._get_property(:instance)}"}}
+
+  # DEBUG: This gets the instance ID, which in the case of a disk is the path to the physical disk.
+  #  I ended up using this later.
+  #  TODO: Test with multiple disks attached to a host. I'm going to setup an NFS virtual 
+  #  appliance and attach it to my local ESX test host, then deploy some VMs to the NFS share.
+  #  I'm using Mike Brown's 'damn small linux' test appliance, it's only about 52MB.
+  #
+
+  # result.each {|r| r.value.each {|s| puts "DEBUG #{s.id.instance}"}}
   
   #
   # Annoying, but the timestamps and the associated latency values are stored in different
@@ -152,20 +168,21 @@ perfProperties.each { |entry|
     # Do the array mapping here, old skool style
     index = 0
     
-    r.value[0].value.each {|v|
-      #puts "DEBUG: Index value: #{index}"
-      #puts "Latency: #{v}"
+    r.value.each { |s|
+      s.value.each {|v| 
+        metrics[index][:device] = s.id.instance
 
-      # Push the value to the metrics array of hash objects
-      metrics[index][:metric] = v
+        # Push the value to the metrics array of hash objects
+        metrics[index][:metric] = v
       
-      #puts "DEBUG: New metric: #{metrics[index]}"
-      index += 1    
+        # puts "DEBUG: New metric: #{metrics[index]}"
+        index += 1    
+      }
     }
   } 
   
   puts "################"
-  puts "DATA: ID => #{metricId}; NAME => #{entry[:name]}"
+  puts "DATA: ID => #{metricId}; NAME => #{entry[:name]}: UNITS => #{entry[:units]}"
   metrics.each{ |time,metric| 
     puts "#{time}: #{metric}"
   }
